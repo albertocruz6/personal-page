adf36a37fd674d49e0b2783901768ff487d3f8da
albertocruz6 <alberto.cruz6@upr.edu>
2021-12-14 23:21:54 -0400

    Cleared up images in projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Projects.css"
 import aonLogo from './aonrobotics.png'
-import pesLogo from './pes_logo.png'
+import pesLogo from './peslogo.png'
 
 const Projects = () => {
     return (
@@ -10,12 +10,16 @@ const Projects = () => {
             <div className='project-details'>
                 <div className='project-spacer'/>
                 <div className='project-details-images'>
-                    <a href="http://aonrobotics.com/" id='aon-project-link'>
-                        <img src={aonLogo} alt="aon-logo" id='aon-logo' width='300' height='153'/>
-                    </a>
-                    <a href="https://luz-verde-frontend.herokuapp.com/about" id='pes-logo-link'>
-                        <img src={pesLogo} alt="pes-logo" id='pes-logo' width='300' height='208'/>
-                    </a>
+                    <div className='aon-image'>
+                        <a href="http://aonrobotics.com/" id='aon-project-link'>
+                            <img src={aonLogo} alt="aon-logo" id='aon-logo' width='300' height='153'/>
+                        </a>
+                    </div>
+                    <div className='pes-image'>
+                        <a href="https://luz-verde-frontend.herokuapp.com/about" id='pes-logo-link'>
+                            <img src={pesLogo} alt="pes-logo" id='pes-logo' width='300' height='208'/>
+                        </a>
+                    </div>
                 </div>
                 
             </div>
