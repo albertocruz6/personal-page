ea412e0775776d45e9f820c83d04b5b1e41235af
albertocruz6 <alberto.cruz6@upr.edu>
2022-09-22 10:00:14 -0400

    Changed some details

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import About from "./components/about/About";
 import Footer from "./components/Footer";
 import Home from "./components/home/Home";
 import Projects from "./components/projects/Projects";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { HashRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
 
 
