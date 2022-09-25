9292f913decbbf020ad642ee3ffa9938ca6e0e74
albertocruz6 <alberto.cruz6@upr.edu>
2022-09-25 16:50:10 -0400

    added almost all important details of experience

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Emoji from "../miscelaneous/Emoji"
 import styled, { keyframes } from 'styled-components'
-import pfp from "./pfp.png"
+import pfp from './pfp.png'
+import {ReactComponent as ReactLogo} from './uprm_logo.svg'
 import './About.css'
 
 const About = () => {
@@ -31,24 +32,61 @@ const About = () => {
             </div>
             <div className='about-details'>
                 <div id='MS_image' className='about-details-images'>
-                    <iframe title='MS GIF' src="https://giphy.com/embed/l41YcLtmnhYyUlbfq" width="480" height="288" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                    <iframe title='MS GIF' src="https://giphy.com/embed/l41YcLtmnhYyUlbfq" width="480" height="288" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
                 </div>
-                <div className='about-details-list'>
+                <div className='about-details-list' id='about-list-experience'>
                     <ul>
                         <li>Experience: </li>
                         <ul>
-                            <li>Microsoft SWE Internship 
+                            <li><b>Microsoft SWE Internship</b> 
                                 <ul>
                                     <li><i>May 2022 - August 2022</i></li>
+                                    <li>Implement a dashboard that, by using the Dataverse API, could query the internal development databases and display charts related to performance diagnostics for organizations, applications, and individual users.  </li>
                                 </ul>
                             </li>
-                            <li>Microsoft Explore Internship 
+                            <li><b>Microsoft Explore Internship</b>
                                 <ul>
                                     <li> <i>June 2021 - August 2021</i></li>
+                                    <li>Implement a performance profiling tool into the Dataverse API and make a diagnostic profile that can display execution times of all the subprocesses, plugins libraries and queries to retrieve data. Exposure of the results made available through a custom REST endpoint and visualization achieved utilizing the Speed Scope open-source project. </li>
                                 </ul>
                             </li>
                         </ul>
                     </ul>
+                </div>
+            </div>
+            <div className='about-details'>
+                <div className='about-details-list'>
+                    <ul>
+                        <li><b>University of Puerto Rico Mayagüez Campus IEEE PES: Luz Verde Project</b> 
+                            <ul>
+                                <li><i>May 2021 - Present</i></li>
+                                <li>Web development. Implementation of React front-end to display results of electric controller for solar power grid output information. Data upload using remote database and retrieval utilizing a REST endpoint developed in Flask. </li>
+                            </ul>
+                        </li>
+                        <li><b>University of Puerto Rico Mayagüez Campus: Computer Architecture Course Assistantship</b>
+                            <ul>
+                                <li><i>February 2022 - May 2022</i></li>
+                                <li>Correct tests and assignments and help with the class material alongside Dr. José Navarro</li>
+                            </ul>
+                        </li>
+                        <li><b>University of Puerto Rico Mayagüez Campus: ML/AI Malware Patterns Research</b>
+                            <ul>
+                                <li><i>May 2021 - May 2022</i></li>
+                                <li>Implementing supervised ML/AI algorithms to recognize malware patterns in distributed code</li>
+                                <li>Evaluate and improve current ML/AI algorithms with prospects of implementing unsupervised/neural networks under the mentorship of Dr. Nayda Santiago. </li>
+                            </ul>
+                        </li>
+                        <li><b>University of Puerto Rico Mayagüez Campus: Data Structures Course Teacher Assistantship</b>
+                            <ul>
+                                <li><i>January 2020 - June 2020</i></li>
+                                <li>Explain, design, and send lab tasks and expectations to students after being approved by Dr. Juan O. Lopez. Monitor tests, give tutoring and reinforce student’s skills during office hours and class hours and correct assignments with appropriate feedback.  </li>
+                            </ul>
+                        </li>
+                        
+                    </ul>
+                </div>
+                <div className='about-details-images' id='UPRM_image'>
+                    <ReactLogo width="100%"/>
                 </div>
             </div>
 
